a3bd2a63f56ceca4eb518dd22b24d20f50a011b0
Muneeb Ul Haq Khan <73172919+12muneeb@users.noreply.github.com>
2024-02-06 10:10:11 +0500

    some changes

--- src/components/CTextField.js
+++ src/components/CTextField.js
@@ -1,44 +1,20 @@
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import React from 'react';
+import { StyleSheet, TouchableOpacity, View } from 'react-native';
 import { TextInput } from 'react-native-paper';
-// import { ImageSource } from 'react-native-vector-icons/Icon';
-// import { themes } from '../../theme/colors';
-// import { CText } from '../../uiComponents'
-// import GlobalStyle from '../../assets/stylings/GlobalStyle';
-// import { icons } from '../../assets/imgs';
 import { responsiveFontSize, responsiveScreenHeight } from 'react-native-responsive-dimensions';
-import CustomText from './CustomText';
-import { colors, size } from '../utils';
 import { appIcons } from '../assets';
+import { colors, size } from '../utils';
+import CustomText from './CustomText';
 
 const CTextfield = props => {
   const [color, setColor] = React.useState(colors.white);
   const {
     inputContainerStyle,
-    inputInnerContainerStyle,
     inputLabel,
-    inputLabelStyle,
-    inputSubLabel,
-    inputSubLabelStyle,
     type,
     onPress,
-    selectedCountry,
-    textStyle,
-    disabled,
-    leftIconName,
-    toggleLeftIconFunc,
-    leftIconButtonStyle,
-    iconStyle,
-    inputErrorStyle,
     error,
-    toggleRightIconFunc,
-    rightIconButtonStyle,
-    rightIconName,
-    rightButton,
-    style,
     value,
-    countryView,
-    countryViewLoading = false,
     placeholder,
     secureTextEntry,
     placeholderTextColor,
@@ -51,95 +27,76 @@ const CTextfield = props => {
     bgColor,
     toggleSecure,
     activeOutlineColor
-
   } = props;
 
-
   const renderErrorView = () => {
-    return (
-    //   <CText style={{ ...GlobalStyle.errorTextStyle, ...inputErrorStyle }}>
-    //     {error}
-    //   </CText>
-    <CustomText text={error} color={colors.white} size={size.small} />
-    );
+    return <CustomText text={error} color={colors.red} size={size.small} />;
   };
+
   const renderInputView = () => {
     return (
-      <TextInput
-        value={value}
-        label={inputLabel}
-        placeholder={placeholder}
-        placeholderTextColor={placeholderTextColor}
-        mode={mode}
-        multiline={multiLine}
-        numberOfLines={numberOfLines}
-        left={
-          icon && (
-            <TextInput.Icon
-              icon={icon}
-              iconColor={iconColor}
-              style={styles.leftIcon}
-              size={responsiveFontSize(2.6)}
-            />
-          )
-        }
-        theme={{
-            colors:{
-                onSurfaceVariant:colors?.white,
-                placeholder:'#fff',
-                
+      <View>
+        <TextInput
+          value={value}
+          label={inputLabel}
+          placeholder={placeholder}
+          placeholderTextColor={placeholderTextColor}
+          mode={mode}
+          multiline={multiLine}
+          numberOfLines={numberOfLines}
+          left={
+            icon && (
+              <TextInput.Icon
+                icon={icon}
+                iconColor={iconColor}
+                style={styles.leftIcon}
+                size={responsiveFontSize(2.6)}
+              />
+            )
+          }
+          theme={{
+            colors: {
+              onSurfaceVariant: colors?.white,
+              placeholder: '#fff',
             },
-            
-        
-        }}
-        activeOutlineColor={activeOutlineColor}
-        outlineColor={outlineColor}
-        outlineStyle={{ borderRadius: 10 }}
-        style={[styles.inputField, bgColor]}
-        textColor={colors?.white}
-        secureTextEntry={secureTextEntry}
-        // onFocus={() => {
-        //   setColor(colo)
-        // }}
-        right={
-          secureTextEntry && (
-            <TextInput.Icon
-              icon={secureTextEntry ? appIcons.eye : appIcons.eyeNot}
-            //   iconColor={themes['light'].colors.grey}
-              onPress={toggleSecure}
-              size={responsiveFontSize(2.6)}
-              style={styles.rightIcon}
-            />
-          )
-        }
-      />
+          }}
+          activeOutlineColor={activeOutlineColor}
+          outlineColor={outlineColor}
+          outlineStyle={{ borderRadius: 10 }}
+          style={[styles.inputField, bgColor]}
+          textColor={colors?.white}
+          secureTextEntry={secureTextEntry}
+          onFocus={() => {
+            setColor(colors.yellow);
+          }}
+          right={
+            secureTextEntry && (
+              <TextInput.Icon
+                icon={secureTextEntry ? appIcons.eye : appIcons.eyeNot}
+                onPress={toggleSecure}
+                size={responsiveFontSize(2.6)}
+                style={styles.rightIcon}
+              />
+            )
+          }
+        />
+        {error ? renderErrorView() : null}
+      </View>
     );
   };
+
   const renderSelectionView = () => {
     return (
-      <TouchableOpacity
-        style={[
-        //   { ...GlobalStyle.inputStyle, ...style },
-          { justifyContent: 'center' },
-        ]}
-        onPress={onPress}>
-        {/* <CText
-          style={[
-            { ...GlobalStyle.inputTextStyle, ...textStyle },
-            !value && { color: themes['light'].colors.bgBlue },
-          ]}>
-          {value ? value : placeholder}
-        </CText> */}
-        <CustomText text= {value ? value : placeholder} color={colors.white}/>
+      <TouchableOpacity style={[{ justifyContent: 'center' }]} onPress={onPress}>
+        <CustomText text={value ? value : placeholder} color={colors.white} />
       </TouchableOpacity>
     );
   };
+
   return (
-    <View style={{  ...inputContainerStyle, }}>
-   
+    <View style={{ ...inputContainerStyle }}>
       {type === 'view' ? renderSelectionView() : renderInputView()}
-   
-      {error ? renderErrorView() : null}
+      {error ? renderErrorView() : null} 
     </View>
   );
 };
@@ -150,20 +107,17 @@ const styles = StyleSheet.create({
   leftIcon: {
     borderRightWidth: 1,
     borderRadius: 0,
-    // borderColor: themes['light'].colors.grey,
-    borderColor:colors.white,
+    borderColor: colors.white,
     paddingRight: 8,
-    marginTop: responsiveScreenHeight(1.4)
+    marginTop: responsiveScreenHeight(1.4),
   },
   rightIcon: {
-    marginTop: responsiveScreenHeight(1.4)
+    marginTop: responsiveScreenHeight(1.4),
   },
   inputField: {
-    // backgroundColor: themes['light'].colors.white,
-    backgroundColor:colors.lightpurple,
+    backgroundColor: colors.lightpurple,
     marginTop: 5,
     height: responsiveScreenHeight(6),
-    color:colors.white
-    
-  }
+    color: colors.white,
+  },
 });
--- src/screens/Auth/Login/Login.js
+++ src/screens/Auth/Login/Login.js
@@ -1,144 +1,155 @@
-import { Formik } from 'formik';
-import React, { Component, createRef } from 'react';
-import {
-  Image,
-  TouchableOpacity,
-  View
-} from 'react-native';
+import React, { Component } from 'react';
+import { Alert, Image, TouchableOpacity, View } from 'react-native';
 import { connect } from 'react-redux';
 import { appIcons, appLogos } from '../../../assets/index';
 import CustomBackground from '../../../components/CustomBackground';
 import CustomButton from '../../../components/CustomButton';
 import CustomText from '../../../components/CustomText';
-import { loginCurrentUser } from '../../../redux/actions/authAction';
 import { colors, family, size } from '../../../utils';
-import { loginValidations } from '../../../utils/validation';
 import styles from './styles';
 import NavService from '../../../helpers/NavService';
 import CTextfield from '../../../components/CTextField';
+import Toast from 'react-native-toast-message';
 
 class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      email: '',
+      password: '',
       verificationPopUp: false,
     };
-    this.loginForm = createRef(null);
   }
 
-
-
+ 
   render() {
     const { email, password } = this.state;
     const OnCreate = () => {
       NavService.navigate('Signup');
+    };
+   const onSubmit = () => {
+      const { email, password } = this.state;
+    
+      if (!email) {
+        Toast.show({
+          text1: 'Email address field can`t be empty.',
+          type: 'error',
+          visibilityTime: 3000,
+        });
+      } 
+      else if (!EmailValidator.validate(email)) {
+        Toast.show({
+          text1: 'Please enter a valid email address.',
+          type: 'error',
+          visibilityTime: 3000,
+        });
+      }else if(!password){
+        Toast.show({
+          text1: 'Password field can`t be empty.',
+          type: 'error',
+          visibilityTime: 3000,
+        });
+      }
+      else{
+Alert.alert('ss')
+      }
     }
     const onForget = () => {
-      NavService.navigate('ForgotPassword')
-    }
+      NavService.navigate('ForgotPassword');
+    };
+
     return (
       <CustomBackground
         showLogo={false}
-        onBack={() => this.props.navigation.goBack()}>
+        onBack={() => this.props.navigation.goBack()}
+      >
         <View style={styles.container}>
-          <Formik
-            innerRef={this.loginForm}
-            onSubmit={values => this.onSubmit(values)}
-            initialValues={{
-              email: '',
-              password: '',
-            }}
-            validationSchema={loginValidations}>
-            {({ handleChange, values, handleSubmit, errors, resetForm }) => {
-              return (
-                <View style={[styles.container, { marginTop: 20 }]}>
-                  <View style={styles.logoStyle}>
-                    <Image style={styles.applogo} source={appLogos.appLogo} />
-                  </View>
-                  <View style={styles.content}>
-                    <CustomText
-                      text="Welcome back!"
-                      color={colors.white}
-                      size={size.h4}
-                      font={family.SofiaProBold}
-                    />
-                    <View
-                      style={styles.subcontent}>
-                      <CustomText
-                        text="Login below or"
-                        color={colors.white}
-                        size={size.small}
-                        font={family.SofiaProRegular}
-                      />
-                      <TouchableOpacity activeOpacity={0.8} onPress={OnCreate}>
-                        <CustomText
-                          text=" Create an Account"
-                          color={colors.primary}
-                          size={size.small}
-                          font={family.SofiaProBold}
-                          style={styles.textStyle}
-                        />
-                      </TouchableOpacity>
-                    </View>
-                  </View>
-                  <View style={styles.postion}>
-                    <CTextfield
-                      ref={email}
-                      secureTextEntry={false}
-                      inputLabel='Email'
-                      placeholder='email@example.com'
-                      placeholderTextColor={colors.white}
-                      mode={'outlined'}
-                      multiLine={false}
-                      numberOfLines={1}
-                      icon={appIcons?.email}
-                      iconColor={colors.primary}
-                      outlineColor={colors.white}
-                      bgColor={{ color: colors.white }}
-                      activeOutlineColor={colors.primary}
-                      values={values?.email}
-                      error={errors?.email}
-                    />
-                    <CTextfield
-                      ref={password}
-                      secureTextEntry={true}
-                      inputLabel='Passoword'
-                      placeholderTextColor={colors.white}
-                      mode={'outlined'}
-                      multiLine={false}
-                      numberOfLines={1}
-                      icon={appIcons?.lock}
-                      iconColor={colors.primary}
-                      outlineColor={colors.white}
-                      bgColor={{ color: colors.white }}
-                      activeOutlineColor={colors.primary}
-                      values={values?.password}
-                      error={errors?.password}
-                    />
-                    <TouchableOpacity activeOpacity={0.8} onPress={onForget} style={styles.forgot}>
-                      <CustomText
-                        text="Forgot Password?"
-                        color={colors.white}
-                        size={size.small}
-                        font={family.SofiaProBold}
-                        style={styles.textStyle}
-                      />
-                    </TouchableOpacity>
-                    <CustomButton
-                      title="Sign in"
-                      onPress={() => handleSubmit()}
-                      buttonStyle={styles.loginBtn}
-                      textStyle={styles.loginTitle}
-                    />
-                  </View>
-                </View>
-              );
-            }}
-          </Formik>
+          <View style={[styles.container, { marginTop: 20 }]}>
+            <View style={styles.logoStyle}>
+              <Image style={styles.applogo} source={appLogos.appLogo} />
+            </View>
+            <View style={styles.content}>
+              <CustomText
+                text="Welcome back!"
+                color={colors.white}
+                size={size.h4}
+                font={family.SofiaProBold}
+              />
+              <View style={styles.subcontent}>
+                <CustomText
+                  text="Login below or"
+                  color={colors.white}
+                  size={size.small}
+                  font={family.SofiaProRegular}
+                />
+                <TouchableOpacity activeOpacity={0.8} onPress={OnCreate}>
+                  <CustomText
+                    text=" Create an Account"
+                    color={colors.primary}
+                    size={size.small}
+                    font={family.SofiaProBold}
+                    style={styles.textStyle}
+                  />
+                </TouchableOpacity>
+              </View>
+            </View>
+            <View style={styles.postion}>
+              <CTextfield
+                inputLabel="Email"
+                placeholder="email@example.com"
+                placeholderTextColor={colors.white}
+                mode={'outlined'}
+                multiLine={false}
+                numberOfLines={1}
+                icon={appIcons?.email}
+                iconColor={colors.primary}
+                outlineColor={colors.white}
+                bgColor={{ color: colors.white }}
+                activeOutlineColor={colors.primary}
+                values={email}
+                onChangeText={(text) => this.setState({ email: text })}
+              />
+              <CTextfield
+                secureTextEntry={true}
+                inputLabel="Password"
+                placeholderTextColor={colors.white}
+                mode={'outlined'}
+                multiLine={false}
+                numberOfLines={1}
+                icon={appIcons?.lock}
+                iconColor={colors.primary}
+                outlineColor={colors.white}
+                bgColor={{ color: colors.white }}
+                activeOutlineColor={colors.primary}
+                values={password}
+                onChangeText={(text) => this.setState({ password: text })}
+              />
+              <TouchableOpacity
+                activeOpacity={0.8}
+                onPress={onForget}
+                style={styles.forgot}
+              >
+                <CustomText
+                  text="Forgot Password?"
+                  color={colors.white}
+                  size={size.small}
+                  font={family.SofiaProBold}
+                  style={styles.textStyle}
+                />
+              </TouchableOpacity>
+              <CustomButton
+                title="Sign in"
+                onPress={onSubmit}
+                buttonStyle={styles.loginBtn}
+                textStyle={styles.loginTitle}
+              />
+            </View>
+          </View>
         </View>
       </CustomBackground>
     );
   }
 }
-const actions = { loginCurrentUser };
-export default connect(null, actions)(Login);
+
+// const actions = { loginCurrentUser };
+export default connect()(Login);
